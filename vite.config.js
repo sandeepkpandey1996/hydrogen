e4e3b4327fe87e4f00bcb580b6718bf79dbec976
sandeepkpandey1996 <sandeep.pandey@kloctechnologies.com>
2025-03-14 21:14:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+
 export default defineConfig({
   plugins: [
     hydrogen(),
